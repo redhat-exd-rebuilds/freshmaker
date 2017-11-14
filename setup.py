1728728a25d6cd24f6c7468068a191a8094a36f0
Jan Kaluza <jkaluza@redhat.com>
2017-11-14 16:09:05 +0100

    Version 0.0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('test-requirements.txt') as f:
 
 setup(name='freshmaker',
       description='Continuous Compose Service',
-      version='0.0.6',
+      version='0.0.7',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
