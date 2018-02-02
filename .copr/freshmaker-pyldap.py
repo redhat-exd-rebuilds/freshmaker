7e489e7c299974d08f7d20b323f50e8f283e0a7c
Jan Kaluza <jkaluza@redhat.com>
2018-02-02 11:39:14 +0100

    Add .copr directory with scripts to allow rebuilding Freshmaker after commit in Copr.

---
+++ .copr/freshmaker-pyldap.py
@@ -0,0 +1,12 @@
+diff --git a/requirements.txt b/requirements.txt
+index 994c785..059aad8 100644
+--- a/requirements.txt
++++ b/requirements.txt
+@@ -22,6 +22,6 @@ enum34
+ odcs[client]
+ krbcontext
+ dogpile.cache
+-pyldap
++python-ldap
+ 
+ git+https://pagure.io/koji.git@koji-1.15.0#egg=koji
