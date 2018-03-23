5b430080eedeae7729f166c0eeccdfba1fd9ac53
Jan Kaluza <jkaluza@redhat.com>
2018-03-23 10:24:17 +0100

    Remove koji from requirements when building COPR package, because Koji in EPEL7 does not provide the python egg

--- .copr/freshmaker-pyldap.py
+++ .copr/freshmaker-pyldap.py
@@ -1,7 +1,7 @@
 diff --git a/requirements.txt.enum34 b/requirements.txt.enum34
 index d9e8779..994b6f6 100644
---- a/requirements.txt.enum34
-+++ b/requirements.txt.enum34
+--- a/requirements.txt
++++ b/requirements.txt
 @@ -22,5 +22,5 @@ enum34 ; python_version <= '2.7'
  odcs[client]
  krbcontext
