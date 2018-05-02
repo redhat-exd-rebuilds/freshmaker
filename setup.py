da5050fa44054fb6bfea440e5b183d51872f6aca
Jan Kaluza <jkaluza@redhat.com>
2018-05-02 12:40:28 +0200

    New version 0.1.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if _:
 
 setup(name='freshmaker',
       description='Continuous Compose Service',
-      version='0.0.15',
+      version='0.1.0',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
