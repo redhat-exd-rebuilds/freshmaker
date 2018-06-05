1e1de937e784ad9344ab2c9d7fa52d9bedcfd0da
Jan Kaluza <jkaluza@redhat.com>
2018-06-05 10:25:23 +0200

    fix copr spec file patch

--- .copr/freshmaker-pyldap.py
+++ .copr/freshmaker-pyldap.py
@@ -1,8 +1,8 @@
-diff --git a/requirements.txt.enum34 b/requirements.txt.enum34
-index d9e8779..994b6f6 100644
+diff --git a/requirements.txt b/requirements.txt
+index 9fb2493..731b95b 100644
 --- a/requirements.txt
 +++ b/requirements.txt
-@@ -22,5 +22,5 @@ enum34 ; python_version <= '2.7'
+@@ -22,6 +22,6 @@ enum34 ; python_version <= '2.7'
  odcs[client]
  krbcontext
  dogpile.cache
