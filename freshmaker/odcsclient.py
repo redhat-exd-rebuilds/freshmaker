9a90931859a7cfab108b9782b3450409728ed531
Jan Kaluza <jkaluza@redhat.com>
2018-11-13 08:55:48 +0100

    Move ODCS code from ErrataAdvisoryRPPMsSignedhandler to FreshmakerODCSClient.
    Currently, half of the ErrataAdvisoryRPMsSignedHandler is code to ask for various
    ODCS composes. This is wrong from following reasons:
    
    - The ODCS related code is not special for this particular handler and might be
      used in the future by other handlers.
    - It makes ErrataAdvisoryRPMsSignedHandler longer than needed and therefore
      harder to read/understand.
    - It makes the tests for ErrataAdvisoryRPMsSignedHandler longer and harder
      to understand.
    
    This commit also removes the "boot.iso" compose type from Freshmaker, because we
    have workarounded the boot.iso generation and we have no plans to support boot.iso
    generation in ODCS ever because of its complexity.
    
    There should be follow-up of this commit to further change the new FreshmakerODCSClient
    API to name the methods better and make them even more general, but for simplicity,
    it is not done in this commit - this commit only moves the code around without
    changing it.

--- freshmaker/odcsclient.py
+++ freshmaker/odcsclient.py
@@ -30,12 +30,22 @@
 # it would import freshmaker.handlers.odcs, so instead, we import it here
 # and in freshmaker.handler do "from freshmaker.odcsclient import ODCS".
 
+import koji
 import os
+import kobo.rpmlib
+
 from odcs.client.odcs import AuthMech, ODCS
-from odcs.common.types import COMPOSE_STATES  # noqa
+from odcs.common.types import COMPOSE_STATES
 from requests.exceptions import HTTPError
 
-from freshmaker import conf, log
+from freshmaker import conf, log, db
+from freshmaker.models import Compose
+from freshmaker.errata import Errata
+from freshmaker.kojiservice import koji_service
+from freshmaker.consumer import work_queue_put
+from freshmaker.types import ArtifactBuildState
+from freshmaker.utils import krb_context, retry
+from freshmaker.events import ODCSComposeStateChangeEvent
 
 
 class RetryingODCS(ODCS):
@@ -71,3 +81,294 @@ def create_odcs_client():
         raise ValueError(
             'Authentication mechanism {0} is not supported yet.'.format(
                 conf.odcs_auth_mech))
+
+
+class FreshmakerODCSClient(object):
+    """
+    Class wrapping ODCS providing high-level methods to generate ODCS composes.
+    This class is intended to be used in the BaseHandler scope.
+    """
+
+    # Used to generate incremental compose id in dry run mode.
+    _FAKE_COMPOSE_ID = 0
+
+    def __init__(self, handler):
+        """
+        Creates new FreshmakerODCSClient.
+
+        :param BaseHandler handler: Handler with which is the newly created
+            instance associated.
+        """
+        self.handler = handler
+
+    def _fake_odcs_new_compose(
+            self, compose_source, tag, packages=None, results=[]):
+        """
+        Fake odcs.new_compose(...) method used in the dry run mode.
+
+        Logs the arguments and emits fake ODCSComposeStateChangeEvent
+
+        :rtype: dict
+        :return: Fake odcs.new_compose dict.
+        """
+        self.handler.log_info(
+            "DRY RUN: Calling fake odcs.new_compose with args: %r",
+            (compose_source, tag, packages, results))
+
+        # Generate the new_compose dict.
+        FreshmakerODCSClient._FAKE_COMPOSE_ID -= 1
+        new_compose = {}
+        new_compose['id'] = FreshmakerODCSClient._FAKE_COMPOSE_ID
+        new_compose['result_repofile'] = "http://localhost/%d.repo" % (
+            new_compose['id'])
+        new_compose['state'] = COMPOSE_STATES['done']
+        if results:
+            new_compose['results'] = ['boot.iso']
+
+        # Generate and inject the ODCSComposeStateChangeEvent event.
+        event = ODCSComposeStateChangeEvent(
+            "fake_compose_msg", new_compose)
+        event.dry_run = True
+        self.handler.log_info("Injecting fake event: %r", event)
+        work_queue_put(event)
+
+        return new_compose
+
+    def _get_packages_for_compose(self, nvr):
+        """Get RPMs of current build NVR
+
+        :param str nvr: build NVR.
+        :return: list of RPM names built from given build.
+        :rtype: list
+        """
+        with koji_service(
+                conf.koji_profile, log, dry_run=self.handler.dry_run) as session:
+            rpms = session.get_build_rpms(nvr)
+        return list(set([rpm['name'] for rpm in rpms]))
+
+    def _get_compose_source(self, nvr):
+        """Get tag from which to collect packages to compose
+        :param str nvr: build NVR used to find correct tag.
+        :return: found tag. None is returned if build is not the latest build
+            of found tag.
+        :rtype: str
+        """
+        with koji_service(
+                conf.koji_profile, log, dry_run=self.handler.dry_run) as service:
+            # Get the list of *-candidate tags, because packages added into
+            # Errata should be tagged into -candidate tag.
+            tags = service.session.listTags(nvr)
+            candidate_tags = [tag['name'] for tag in tags
+                              if tag['name'].endswith('-candidate')]
+
+            # Candidate tags may include unsigned packages and ODCS won't
+            # allow generating compose from them, so try to find out final
+            # version of candidate tag (without the "-candidate" suffix).
+            final_tags = []
+            for candidate_tag in candidate_tags:
+                final = candidate_tag[:-len("-candidate")]
+                final_tags += [tag['name'] for tag in tags
+                               if tag['name'] == final]
+
+            # Prefer final tags over candidate tags.
+            tags_to_try = final_tags + candidate_tags
+            for tag in tags_to_try:
+                latest_build = service.session.listTagged(
+                    tag,
+                    latest=True,
+                    package=koji.parse_NVR(nvr)['name'])
+                if latest_build and latest_build[0]['nvr'] == nvr:
+                    self.handler.log_info(
+                        "Package %r is latest version in tag %r, "
+                        "will use this tag", nvr, tag)
+                    return tag
+                elif not latest_build:
+                    self.handler.log_info(
+                        "Could not find package %r in tag %r, "
+                        "skipping this tag", nvr, tag)
+                else:
+                    self.handler.log_info(
+                        "Package %r is not he latest in the tag %r ("
+                        "latest is %r), skipping this tag",
+                        nvr, tag, latest_build[0]['nvr'])
+
+    def prepare_yum_repos_for_rebuilds(self, db_event):
+        repo_urls = []
+        db_composes = []
+
+        compose = self.prepare_yum_repo(db_event)
+        db_composes.append(Compose(odcs_compose_id=compose['id']))
+        db.session.add(db_composes[-1])
+        repo_urls.append(compose['result_repofile'])
+
+        for dep_event in db_event.find_dependent_events():
+            compose = self.prepare_yum_repo(dep_event)
+            db_composes.append(Compose(odcs_compose_id=compose['id']))
+            db.session.add(db_composes[-1])
+            repo_urls.append(compose['result_repofile'])
+
+        # commit all new composes
+        db.session.commit()
+
+        for build in db_event.builds:
+            build.add_composes(db.session, db_composes)
+        db.session.commit()
+
+        # Remove duplicates from repo_urls.
+        return list(set(repo_urls))
+
+    def prepare_yum_repo(self, db_event):
+        """
+        Request a compose from ODCS for builds included in Errata advisory
+
+        Run a compose in ODCS to contain required RPMs for rebuilding images
+        later.
+
+        :param Event db_event: current event being handled that contains errata
+            advisory to get builds containing updated RPMs.
+        :return: a mapping returned from ODCS that represents the request
+            compose.
+        :rtype: dict
+        """
+        errata_id = int(db_event.search_key)
+
+        packages = []
+        errata = Errata()
+        builds = errata.get_builds(errata_id)
+        compose_source = None
+        for nvr in builds:
+            packages += self._get_packages_for_compose(nvr)
+            source = self._get_compose_source(nvr)
+            if compose_source and compose_source != source:
+                # TODO: Handle this by generating two ODCS composes
+                db_event.builds_transition(
+                    ArtifactBuildState.FAILED.value, "Packages for errata "
+                    "advisory %d found in multiple different tags."
+                    % (errata_id))
+                return
+            else:
+                compose_source = source
+
+        if compose_source is None:
+            db_event.builds_transition(
+                ArtifactBuildState.FAILED.value, 'None of builds %s of '
+                'advisory %d is the latest build in its candidate tag.'
+                % (builds, errata_id))
+            return
+
+        self.handler.log_info(
+            'Generating new compose for rebuild: '
+            'source: %s, source type: %s, packages: %s',
+            compose_source, 'tag', packages)
+
+        if not self.handler.dry_run:
+            with krb_context():
+                new_compose = create_odcs_client().new_compose(
+                    compose_source, 'tag', packages=packages,
+                    sigkeys=conf.odcs_sigkeys, flags=["no_deps"])
+        else:
+            new_compose = self._fake_odcs_new_compose(
+                compose_source, 'tag', packages=packages)
+
+        return new_compose
+
+    def prepare_pulp_repo(self, build, content_sets):
+        """
+        Prepares .repo file containing the repositories matching
+        the content_sets by creating new ODCS compose of PULP type.
+
+        This currently blocks until the compose is done or failed.
+
+        :param build: models.ModuleBuild instance associated with this compose.
+        :param list content_sets: List of content sets.
+        :rtype: dict
+        :return: ODCS compose dictionary.
+        """
+        self.handler.log_info(
+            'Generating new PULP type compose for content_sets: %r',
+            content_sets)
+
+        odcs = create_odcs_client()
+        if not self.handler.dry_run:
+            with krb_context():
+                new_compose = odcs.new_compose(
+                    ' '.join(content_sets), 'pulp')
+
+                # Pulp composes in ODCS takes just few seconds, because ODCS
+                # only generates the .repo file after single query to Pulp.
+                # TODO: Freshmaker is currently not designed to handle
+                # multiple ODCS composes per rebuild Event and since these
+                # composes are done in no-time normally, it is OK here to
+                # block. It would still be nice to redesign that part of
+                # Freshmaker to do things "right".
+                # This is tracked here: https://pagure.io/freshmaker/issue/114
+                @retry(timeout=60, interval=2)
+                def wait_for_compose(compose_id):
+                    ret = odcs.get_compose(compose_id)
+                    if ret["state_name"] == "done":
+                        return True
+                    elif ret["state_name"] == "failed":
+                        return False
+                    self.handler.log_info(
+                        "Waiting for Pulp compose to finish: %r", ret)
+                    raise Exception("ODCS compose not finished.")
+
+                done = wait_for_compose(new_compose["id"])
+                if not done:
+                    build.transition(
+                        ArtifactBuildState.FAILED.value, "Cannot generate "
+                        "ODCS PULP compose %s for content_sets %r"
+                        % (str(new_compose["id"]), content_sets))
+        else:
+            new_compose = self._fake_odcs_new_compose(
+                content_sets, 'pulp')
+
+        return new_compose
+
+    def prepare_odcs_compose_with_image_rpms(self, image):
+        """
+        Request a compose from ODCS for builds included in Errata advisory
+
+        Run a compose in ODCS to contain required RPMs for rebuilding images
+        later.
+
+        :param dict image: Container image representation as returned by
+            LightBlue class.
+        :return: a mapping returned from ODCS that represents the request
+            compose.
+        :rtype: dict
+        """
+
+        if not image.get('rpm_manifest'):
+            self.handler.log_warn('"rpm_manifest" not set in image.')
+            return
+
+        rpm_manifest = image["rpm_manifest"][0]
+        if not rpm_manifest.get('rpms'):
+            return
+
+        builds = set()
+        packages = set()
+        for rpm in rpm_manifest["rpms"]:
+            parsed_nvr = kobo.rpmlib.parse_nvra(rpm["srpm_nevra"])
+            srpm_nvr = "%s-%s-%s" % (parsed_nvr["name"], parsed_nvr["version"],
+                                     parsed_nvr["release"])
+            builds.add(srpm_nvr)
+            parsed_nvr = kobo.rpmlib.parse_nvra(rpm["nvra"])
+            packages.add(parsed_nvr["name"])
+
+        if not self.handler.dry_run:
+            with krb_context():
+                new_compose = create_odcs_client().new_compose(
+                    "", 'build', packages=packages, builds=builds,
+                    sigkeys=conf.odcs_sigkeys, flags=["no_deps"])
+        else:
+            new_compose = self._fake_odcs_new_compose(
+                "", 'build', packages=packages,
+                builds=builds)
+
+        self.handler.log_info(
+            "Started generating ODCS 'build' type compose %d." % (
+                new_compose["id"]))
+
+        return new_compose
--- tests/test_errata_advisory_rpms_signed_handler.py
+++ tests/test_errata_advisory_rpms_signed_handler.py
@@ -19,8 +19,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import requests
-
 from mock import patch
 
 import freshmaker
@@ -56,18 +54,11 @@ class TestErrataAdvisoryRPMsSignedHandler(helpers.ModelsTestCase):
             'freshmaker.messaging.publish')
 
         self.mock_prepare_pulp_repo = self.patcher.patch(
-            '_prepare_pulp_repo',
+            'freshmaker.odcsclient.FreshmakerODCSClient.prepare_pulp_repo',
             side_effect=[{'id': compose_id} for compose_id in range(1, 7)])
 
         self.mock_find_images_to_rebuild = self.patcher.patch(
             '_find_images_to_rebuild')
-
-        # boot.iso composes IDs should be different from pulp composes IDs as
-        # when each time to request a compose from ODCS, new compose ID will
-        # be returned along with new comopse.
-        self.mock_request_boot_iso_compose = self.patcher.patch(
-            '_request_boot_iso_compose',
-            side_effect=[{'id': 100}, {'id': 101}])
 
         # Fake images found to rebuild has these relationships
         #
@@ -370,8 +361,7 @@ class TestErrataAdvisoryRPMsSignedHandler(helpers.ModelsTestCase):
 
     @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
            'allow_build', return_value=True)
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_prepare_yum_repos_for_rebuilds')
+    @patch('freshmaker.odcsclient.FreshmakerODCSClient.prepare_yum_repos_for_rebuilds')
     @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
            'start_to_build_images')
     def test_rebuild_if_errata_state_is_prior_to_SHIPPED_LIVE(
@@ -388,8 +378,7 @@ class TestErrataAdvisoryRPMsSignedHandler(helpers.ModelsTestCase):
 
     @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
            'allow_build', return_value=True)
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_prepare_yum_repos_for_rebuilds')
+    @patch('freshmaker.odcsclient.FreshmakerODCSClient.prepare_yum_repos_for_rebuilds')
     @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
            'start_to_build_images')
     @patch('freshmaker.models.Event.get_image_builds_in_first_batch')
@@ -410,201 +399,6 @@ class TestErrataAdvisoryRPMsSignedHandler(helpers.ModelsTestCase):
 
         db_event = Event.get(db.session, event.msg_id)
         self.assertEqual(EventState.BUILDING.value, db_event.state)
-
-
-class TestGetBaseImageBuildTarget(helpers.FreshmakerTestCase):
-    """Test ErrataAdvisoryRPMsSignedHandler._get_base_image_build_target"""
-
-    def setUp(self):
-        super(TestGetBaseImageBuildTarget, self).setUp()
-
-        self.image = ContainerImage({
-            'repository': 'repo_1',
-            'commit': '1234567',
-            'target': 'docker-container-candidate',
-            'git_branch': 'rhel-7.4',
-            'content_sets': ['image_a_content_set_1', 'image_a_content_set_2'],
-            'brew': {
-                'build': 'image-a-1.0-2',
-            },
-            'parent': None,
-            'parsed_data': {
-                'layers': [
-                    'sha512:7890',
-                    'sha512:5678',
-                ],
-                'files': [
-                    {
-                        'filename': 'Dockerfile',
-                        'content_url': 'http://pkgs.localhost/cgit/rpms/'
-                                       'image-a/plain/Dockerfile?id=fa521323',
-                        'key': 'buildfile'
-                    }
-                ]
-            },
-        })
-        self.handler = ErrataAdvisoryRPMsSignedHandler()
-
-    @patch('requests.get')
-    def test_get_target_from_image_build_conf(self, get):
-        get.return_value.content = '''\
-[image-build]
-name = image-a
-arches = x86_64
-format = docker
-disk_size = 10
-ksurl = git://git.localhost/spin-kickstarts.git?rhel7#HEAD
-kickstart = rhel-7.4-server-docker.ks
-version = 7.4
-target = guest-rhel-7.4-docker
-distro = RHEL-7.4
-ksversion = RHEL7'''
-
-        result = self.handler._get_base_image_build_target(self.image)
-        self.assertEqual('guest-rhel-7.4-docker', result)
-
-    @patch('requests.get')
-    def test_image_build_conf_is_unavailable_in_distgit(self, get):
-        get.return_value.raise_for_status.side_effect = \
-            requests.exceptions.HTTPError('error')
-
-        result = self.handler._get_base_image_build_target(self.image)
-        self.assertIsNone(result)
-
-    @patch('requests.get')
-    def test_image_build_conf_is_empty(self, get):
-        get.return_value.content = ''
-
-        result = self.handler._get_base_image_build_target(self.image)
-        self.assertIsNone(result)
-
-    @patch('requests.get')
-    def test_image_build_conf_is_not_INI(self, get):
-        get.return_value.content = 'abc'
-
-        result = self.handler._get_base_image_build_target(self.image)
-        self.assertIsNone(result)
-
-
-class TestGetBaseImageBuildTag(helpers.FreshmakerTestCase):
-    """Test ErrataAdvisoryRPMsSignedHandler._get_base_image_build_tag"""
-
-    def setUp(self):
-        super(TestGetBaseImageBuildTag, self).setUp()
-
-        self.image = ContainerImage({
-            'repository': 'repo_1',
-            'commit': '1234567',
-            'target': 'docker-container-candidate',
-            'git_branch': 'rhel-7.4',
-            'content_sets': ['image_a_content_set_1', 'image_a_content_set_2'],
-            'brew': {
-                'build': 'image-a-1.0-2',
-            },
-            'parent': None,
-            'parsed_data': {
-                'layers': [
-                    'sha512:7890',
-                    'sha512:5678',
-                ],
-                'files': [
-                    {
-                        'filename': 'Dockerfile',
-                        'content_url': 'http://pkgs.localhost/cgit/rpms/'
-                                       'image-a/plain/Dockerfile?id=fa521323',
-                        'key': 'buildfile'
-                    }
-                ]
-            },
-        })
-        self.handler = ErrataAdvisoryRPMsSignedHandler()
-
-    @helpers.mock_koji
-    def test_get_build_tag_name(self, mocked_koji):
-        result = self.handler._get_base_image_build_tag(
-            'guest-rhel-7.4-docker')
-        self.assertEqual('guest-rhel-7.4-docker-build', result)
-
-    @helpers.mock_koji
-    def test_no_target_is_returned(self, mocked_koji):
-        result = self.handler._get_base_image_build_tag(
-            'guest-rhel-7.4-docker-unknown')
-        self.assertIsNone(result)
-
-
-class TestRequestBootISOCompose(helpers.FreshmakerTestCase):
-    """Test ErrataAdvisoryRPMsSignedHandler._request_boot_iso_compose"""
-
-    def setUp(self):
-        super(TestRequestBootISOCompose, self).setUp()
-
-        self.image = ContainerImage({
-            'repository': 'repo_1',
-            'commit': '1234567',
-            'target': 'docker-container-candidate',
-            'git_branch': 'rhel-7.4',
-            'content_sets': ['image_a_content_set_1', 'image_a_content_set_2'],
-            'brew': {
-                'build': 'image-a-1.0-2',
-            },
-            'parent': None,
-            'parsed_data': {
-                'layers': [
-                    'sha512:7890',
-                    'sha512:5678',
-                ],
-                'files': [
-                    {
-                        'filename': 'Dockerfile',
-                        'content_url': 'http://pkgs.localhost/cgit/rpms/'
-                                       'image-a/plain/Dockerfile?id=fa521323',
-                        'key': 'buildfile'
-                    }
-                ]
-            },
-        })
-        self.handler = ErrataAdvisoryRPMsSignedHandler()
-
-    @patch('freshmaker.handlers.errata.errata_advisory_rpms_signed.'
-           'create_odcs_client')
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_get_base_image_build_target')
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_get_base_image_build_tag')
-    def test_get_boot_iso_compose(
-            self, get_base_image_build_tag, get_base_image_build_target,
-            create_odcs_client):
-        odcs = create_odcs_client.return_value
-        odcs.new_compose.return_value = {'id': 1}
-
-        get_base_image_build_target.return_value = 'build-target'
-        get_base_image_build_tag.return_value = 'build-tag'
-
-        result = self.handler._request_boot_iso_compose(self.image)
-
-        self.assertEqual(odcs.new_compose.return_value, result)
-        odcs.new_compose.assert_called_once_with(
-            'build-tag', 'tag', results=['boot.iso'])
-
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_get_base_image_build_target')
-    def test_cannot_get_image_build_target(self, get_base_image_build_target):
-        get_base_image_build_target.return_value = None
-
-        result = self.handler._request_boot_iso_compose(self.image)
-        self.assertIsNone(result)
-
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_get_base_image_build_target')
-    @patch('freshmaker.handlers.errata.ErrataAdvisoryRPMsSignedHandler.'
-           '_get_base_image_build_tag')
-    def test_cannot_get_build_tag_from_target(
-            self, get_base_image_build_tag, get_base_image_build_target):
-        get_base_image_build_target.return_value = 'build-target'
-        get_base_image_build_tag.return_value = None
-
-        result = self.handler._request_boot_iso_compose(self.image)
-        self.assertIsNone(result)
 
 
 class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
