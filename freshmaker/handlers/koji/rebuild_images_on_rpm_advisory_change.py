redhat-exd-rebuilds/freshmaker

commit 3726cd455ca3a31fba144b9b5a3c42a8b0b24745

Use Errata Tool CVE mapping functionality
Errata now provides an API to get builds by cve for an advisory, it
returns the builds affected by CVEs in advisory, we can use this API to
replace the old implementation of fetching packages affected by CVEs
and comparing the result with builds in advisory.

And we use the "security_impact" value from advisory instead of the
old "highest_cve_severity" which is fetched from sfm2. The
"security_impact" in errata is more precise, because for each CVE,
the severity we fetched from sfm2 is a global value, and for each CVE,
it can has different severity values for different products. So we
don't need the sfm2 module any more.

JIRA: CLOUDWF-2482

freshmaker/handlers/koji/rebuild_images_on_rpm_advisory_change.py
@@ -24,7 +24,6 @@
 
 import json
 import koji
-import kobo
 
 from freshmaker import conf, db
 from freshmaker.events import (
@@ -36,7 +35,6 @@ from freshmaker.errata import Errata
 from freshmaker.types import (
     ArtifactType, ArtifactBuildState, EventState, RebuildReason)
 from freshmaker.models import Event, Compose
-from freshmaker.utils import is_pkg_modular
 
 
 class RebuildImagesOnRPMAdvisoryChange(ContainerBuildHandler):
@@ -413,31 +411,8 @@ class RebuildImagesOnRPMAdvisoryChange(ContainerBuildHandler):
         if isinstance(self.event, ManualRebuildWithAdvisoryEvent):
             leaf_container_images = self.event.container_images
 
-        # For each SRPM NVR, find out all the containers which include
-        # this SRPM NVR.
-        srpm_nvrs = set(errata.get_builds(errata_id))
-        # affected_pkgs contains the list of actually affected pkgs from the CVE.
-        # We don't need to build images that really don't affect the CVE, even if they are
-        # listed in the RHSA. So let's just remove the ones that are not listed in here.
-        # In case this is empty we are just going to use the "old" behavior before this
-        # change was made, and rebuild everything.
-        affected_pkgs = set([pkg['pkg_name'] for pkg in self.event.advisory.affected_pkgs])
-        if affected_pkgs:
-            tmp_srpm_nvrs = set(srpm_nvrs)
-            srpm_nvrs = set()
-            for srpm_nvr in tmp_srpm_nvrs:
-                srpm_name = kobo.rpmlib.parse_nvr(srpm_nvr)["name"]
-                # In case the SRPM NVR is modular, the `affected_pkgs` might contain
-                # modules which are in the "module_name:module_stream/pkg_name" format.
-                # We need to respect this format and only try to match the package name, it
-                # means only the "/pkg_name" part of affected_pkg.
-                if is_pkg_modular(srpm_nvr):
-                    if any(affected_pkg.endswith(f"/{srpm_name}") for affected_pkg in affected_pkgs):
-                        srpm_nvrs.add(srpm_nvr)
-                elif srpm_name in affected_pkgs:
-                    srpm_nvrs.add(srpm_nvr)
-            self.log_info(("Not going to rebuild container images with RPMS from these SRPMs "
-                           "because they're not affected: %r"), tmp_srpm_nvrs.difference(srpm_nvrs))
+        # Get srpm nvrs which are affected by the CVEs in this advisory
+        srpm_nvrs = self.event.advisory.affected_srpm_nvrs
 
         self.log_info(
             "Going to find all the container images to rebuild as "

freshmaker/errata.py
@@ -32,7 +32,6 @@ from freshmaker.events import (
     BrewSignRPMEvent, ErrataBaseEvent,
     FreshmakerManualRebuildEvent)
 from freshmaker import conf, log
-from freshmaker.sfm2 import SFM2API
 from freshmaker.utils import retry
 
 
@@ -56,9 +55,16 @@ class ErrataAdvisory(object):
         self.cve_list = cve_list or []
         self.has_hightouch_bug = has_hightouch_bug
 
-        sfm2 = SFM2API()
+        self._affected_srpm_nvrs = None
 
-        self.highest_cve_severity, self.affected_pkgs = sfm2.fetch_cve_metadata(self.cve_list)
+    @property
+    def affected_srpm_nvrs(self):
+        if self._affected_srpm_nvrs is not None:
+            return self._affected_srpm_nvrs
+
+        errata = Errata()
+        self._affected_srpm_nvrs = errata.get_cve_affected_srpm_nvrs(self.errata_id)
+        return self._affected_srpm_nvrs
 
     @classmethod
     def from_advisory_id(cls, errata, errata_id):
@@ -78,6 +84,12 @@ class ErrataAdvisory(object):
         else:
             cve_list = []
 
+        # security_impact in errata is capitalized string, making it lowercase
+        # for backwards compatibility with SFM2's security impact (we used to
+        # get the severity from SFM2). It's used in our config to allow or block
+        # rebuilds for artifacts.
+        security_impact = erratum_data["security_impact"].lower()
+
         has_hightouch_bug = False
         bugs = errata._get_bugs(erratum_data["id"]) or []
         for bug in bugs:
@@ -87,7 +99,7 @@ class ErrataAdvisory(object):
 
         return ErrataAdvisory(
             erratum_data["id"], erratum_data["fulladvisory"], erratum_data["status"],
-            erratum_data['content_types'], erratum_data["security_impact"],
+            erratum_data['content_types'], security_impact,
             product_data["product"]["short_name"], cve_list,
             has_hightouch_bug)
 
@@ -379,3 +391,26 @@ class Errata(object):
         data = self._errata_http_get(
             '/errata/get_pulp_packages/{}.json'.format(errata_id))
         return data.keys()
+
+    def get_cve_affected_srpm_nvrs(self, errata_id):
+        """ Get SRPM nvrs which are affected by the CVEs in errata
+
+        :param errata_id: Errata advisory ID, e.g. 25713.
+        :type errata_id: str or int
+        :return: a list of strings each of them is a source rpm nvr
+        :rtype: list
+        """
+        data = self._errata_rest_get(f"/erratum/{errata_id}/builds_by_cve")
+        srpms = set()
+        for cve, data_by_product in data.items():
+            for product, product_data in data_by_product.items():
+                for build in product_data.get("builds", []):
+                    for build_nvr, build_info in build.items():
+                        # for rpm advisories, build_nvr equal to srpm nvr, but this is
+                        # not true for module advisories, so we need to get the srpms
+                        # from variants data
+                        for variant, variant_data in build_info.get("variant_arch", {}).items():
+                            for srpm in variant_data.get("SRPMS", []):
+                                srpms.add(srpm.split(".src.rpm")[0])
+
+        return list(srpms)

freshmaker/events.py
@@ -291,7 +291,6 @@ class ErrataBaseEvent(BaseEvent):
             advisory_state=self.advisory.state,
             advisory_name=self.advisory.name,
             advisory_security_impact=self.advisory.security_impact,
-            advisory_highest_cve_severity=self.advisory.highest_cve_severity,
             advisory_product_short_name=self.advisory.product_short_name,
             advisory_has_hightouch_bug=self.advisory.has_hightouch_bug,
             advisory_content_types=' '.join(self.advisory.content_types),

freshmaker/handlers/internal/generate_advisory_signed_event_on_rpm_sign.py
@@ -76,7 +76,6 @@ class GenerateAdvisorySignedEventOnRPMSign(BaseHandler):
                 ArtifactType.IMAGE,
                 advisory_name=advisory.name,
                 advisory_security_impact=advisory.security_impact,
-                advisory_highest_cve_severity=advisory.highest_cve_severity,
                 advisory_state=advisory.state)]
 
         # Filter out advisories which are already in Freshmaker DB.

freshmaker/sfm2.py
@@ -1,125 +1,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2019  Red Hat, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-import requests
-
-from freshmaker import log, conf
-
-
-class SFM2API(object):
-
-    # Ordered Threat severities.
-    THREAT_SEVERITIES = [
-        "low",
-        "moderate",
-        "important",
-        "critical",
-    ]
-
-    def __init__(self, server_url=None):
-        """
-        Creates new SFM2API instance.
-
-        :param str server_url: SFM2API base URL.
-        """
-        if server_url is not None:
-            self.server_url = server_url.rstrip('/')
-        else:
-            self.server_url = conf.sfm2_api_url.rstrip('/')
-
-    def query_sfm2(self, cve):
-        """
-        Queries SFM2 to find out infos about the cve, specifically
-        the CVE impact and list of affected packages
-        It queries api/public/flaws?id=$cve&include_fields=affects,impact endpoint.
-
-        :param str cve: CVE, for example "CVE-2017-10268".
-        :rtype: dict
-        :return: dict with two keys, "impact", and "affects". The first references
-        the impact of the CVE, and the second is a list of dicts representing packages
-        affected by the CVE.
-        """
-        log.debug("Querying SFM2 for %s", cve)
-        r = requests.get(
-            "%s/api/public/flaws" % self.server_url,
-            params={"include_fields": "affects,impact", "id": cve})
-        r.raise_for_status()
-
-        return r.json()[0]
-
-    def fetch_cve_metadata(self, cve_list):
-        """
-        Fetches metadata about each CVE in `cve_list` and returns a tuple with
-        the name of highest severity rate and the affected packages (a dictionary
-        with product and pkg_name).
-        See `SFM2API.THREAT_SEVERITIES` for list of possible severity rates.
-
-        :param list cve_list: List of strings with CVE names.
-        :rtype: str
-        :return: Tuple, the first element is the name of highest severity rate occuring
-        in CVEs from `cve_list`. The second element is a list of dicts, with "product"
-        and "pkg_name" of the affected packages.
-        """
-        max_rating = -1
-        affected_pkgs = []
-        severity = None
-        for cve in cve_list:
-            try:
-                elements = self.query_sfm2(cve)
-            except requests.exceptions.HTTPError as e:
-                if e.response.status_code == 400:
-                    log.warning(
-                        "The request for the CVE %s to the SFM2 API seems wrong, "
-                        "impact and affected packages unknown. %s", cve, e.response.request.url)
-                    continue
-                if e.response.status_code == 500:
-                    log.warning(
-                        "Some error occurred looking forCVE %s with SFM2 API, "
-                        "impact and affected packages unknown. %s", cve, e.response.request.url)
-                    continue
-                raise
-
-            try:
-                severity = elements['impact']
-            except (IndexError, KeyError):
-                log.warning("Some error occured looking for impact for CVE %s using SFM2 API", cve)
-
-            try:
-                affected_pkgs.extend([
-                    {'product': item['ps_module'], 'pkg_name': item['ps_component']}
-                    for item in elements['affects'] if (
-                        item['affected'] != "notaffected" and
-                        item['resolution'] not in ["wontfix", "ooss"])])
-            except (KeyError, IndexError):
-                log.exception("Some error occured looking for affected packages for CVE %s using SFM2 API", cve)
-
-            try:
-                rating = SFM2API.THREAT_SEVERITIES.index(severity)
-            except ValueError:
-                log.error("Unknown threat_severity '%s' for CVE %s",
-                          severity, cve)
-                continue
-
-            max_rating = max(max_rating, rating)
-
-        if max_rating == -1:
-            return (None, affected_pkgs)
-        return (SFM2API.THREAT_SEVERITIES[max_rating], affected_pkgs)

tests/handlers/koji/test_rebuild_images_on_rpm_advisory_change.py
@@ -288,18 +288,18 @@ class TestRebuildImagesOnRPMAdvisoryChange(helpers.ModelsTestCase):
     @patch.object(freshmaker.conf, 'handler_build_whitelist', new={
         'RebuildImagesOnRPMAdvisoryChange': {
             'image': {
-                'advisory_highest_cve_severity': ['critical', 'important']
+                'advisory_security_impact': ['critical', 'important']
             }
         }
     })
     @patch.object(freshmaker.conf, 'dry_run', new=True)
-    def test_allow_build_by_highest_cve_severity(self):
+    def test_allow_build_by_security_impact(self):
         compose_4 = Compose(odcs_compose_id=4)
         db.session.add(compose_4)
         db.session.commit()
 
         for severity in ["moderate", "critical", "important"]:
-            self.rhba_event.advisory.highest_cve_severity = severity
+            self.rhba_event.advisory.security_impact = severity
             self.mock_find_images_to_rebuild.return_value = [[]]
             handler = RebuildImagesOnRPMAdvisoryChange()
             handler.handle(self.rhba_event)
@@ -435,8 +435,8 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
             'freshmaker.errata.Errata.get_pulp_repository_ids',
             return_value=["pulp_repo_x86_64"])
 
-        self.get_builds = self.patcher.patch(
-            'freshmaker.errata.Errata.get_builds',
+        self.get_affected_srpm_nvrs = self.patcher.patch(
+            'freshmaker.errata.Errata.get_cve_affected_srpm_nvrs',
             return_value=["httpd-2.4-11.el7"])
 
         self.find_images_to_rebuild = self.patcher.patch(
@@ -472,7 +472,7 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
             pass
 
         self.find_images_to_rebuild.assert_called_once_with(
-            set(['httpd-2.4-11.el7']), ['content-set-1'],
+            ['httpd-2.4-11.el7'], ['content-set-1'],
             filter_fnc=self.handler._filter_out_not_allowed_builds,
             published=True, release_categories=conf.lightblue_release_categories,
             leaf_container_images=None)
@@ -484,12 +484,12 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
     })
     @patch('os.path.exists', return_value=True)
     def test_multiple_srpms(self, exists):
-        self.get_builds.return_value = ["httpd-2.4-11.el7", "httpd-2.2-11.el6"]
+        self.get_affected_srpm_nvrs.return_value = ["httpd-2.4-11.el7", "httpd-2.2-11.el6"]
         for x in self.handler._find_images_to_rebuild(123456):
             pass
 
         self.find_images_to_rebuild.assert_called_once_with(
-            set(['httpd-2.4-11.el7', 'httpd-2.2-11.el6']), ['content-set-1'],
+            ['httpd-2.4-11.el7', 'httpd-2.2-11.el6'], ['content-set-1'],
             filter_fnc=self.handler._filter_out_not_allowed_builds,
             published=True, release_categories=conf.lightblue_release_categories,
             leaf_container_images=None)
@@ -508,7 +508,7 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
             pass
 
         self.find_images_to_rebuild.assert_called_once_with(
-            set(['httpd-2.4-11.el7']), ['content-set-1'],
+            ['httpd-2.4-11.el7'], ['content-set-1'],
             filter_fnc=self.handler._filter_out_not_allowed_builds,
             published=None, release_categories=None,
             leaf_container_images=None)
@@ -525,7 +525,7 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
             pass
 
         self.find_images_to_rebuild.assert_called_once_with(
-            set(['httpd-2.4-11.el7']), ['content-set-1'],
+            ['httpd-2.4-11.el7'], ['content-set-1'],
             filter_fnc=self.handler._filter_out_not_allowed_builds,
             published=True, release_categories=conf.lightblue_release_categories,
             leaf_container_images=None)
@@ -543,7 +543,7 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
             pass
 
         self.find_images_to_rebuild.assert_called_once_with(
-            set(['httpd-2.4-11.el7']), ['content-set-1'],
+            ['httpd-2.4-11.el7'], ['content-set-1'],
             filter_fnc=self.handler._filter_out_not_allowed_builds,
             published=True, release_categories=conf.lightblue_release_categories,
             leaf_container_images=["foo", "bar"])
@@ -553,37 +553,15 @@ class TestFindImagesToRebuild(helpers.FreshmakerTestCase):
             'image': {'advisory_name': 'RHBA-*'}
         }
     })
+    @patch("freshmaker.errata.ErrataAdvisory.affected_srpm_nvrs",
+           new_callable=PropertyMock,
+           return_value=["nodejs-10.19.0-1.module+el8.1.0+5726+6ed65f8c.x86_64"])
     @patch('os.path.exists', return_value=True)
-    def test_whitelist_affected_packages(self, exists):
-        """
-        In case there are more pkgs in a RHSA, but not all of them are actually affected from the CVE,
-        the images that will have to be rebuild will be only the ones affected.
-        This test is checking this process.
-        """
-        self.event.advisory.affected_pkgs = [{'product': 'whatever', 'pkg_name': 'httpd'}]
-        self.get_builds.return_value = ["httpd-2.4-11.el7", "foo-1-1"]
-        for x in self.handler._find_images_to_rebuild(123456):
-            pass
-
-        self.find_images_to_rebuild.assert_called_once_with(
-            set(['httpd-2.4-11.el7']), ['content-set-1'],
-            filter_fnc=self.handler._filter_out_not_allowed_builds,
-            published=True, release_categories=conf.lightblue_release_categories,
-            leaf_container_images=None)
-
-    @patch.object(freshmaker.conf, 'handler_build_whitelist', new={
-        'RebuildImagesOnRPMAdvisoryChange': {
-            'image': {'advisory_name': 'RHBA-*'}
-        }
-    })
-    @patch('os.path.exists', return_value=True)
-    def test_affected_packages_with_modules(self, exists):
-        self.event.advisory.affected_pkgs = [{'product': 'rhel-8', 'pkg_name': 'nodejs:10/nodejs'}]
-        self.get_builds.return_value = ["nodejs-10.19.0-1.module+el8.1.0+5726+6ed65f8c.x86_64"]
+    def test_affected_packages_with_modules(self, exists, affected_srpm_nvrs):
         self.handler._find_images_to_rebuild(123456)
 
         self.find_images_to_rebuild.assert_called_once_with(
-            set(['nodejs-10.19.0-1.module+el8.1.0+5726+6ed65f8c.x86_64']), ['content-set-1'],
+            ['nodejs-10.19.0-1.module+el8.1.0+5726+6ed65f8c.x86_64'], ['content-set-1'],
             filter_fnc=self.handler._filter_out_not_allowed_builds,
             published=True, release_categories=conf.lightblue_release_categories,
             leaf_container_images=None)

tests/test_errata.py
@@ -131,10 +131,59 @@ class MockedErrataAPI(object):
         self.product_versions[3] = {"rhel_release": {"name": "RHEL-6-foobar"}}
         self.product_versions[4] = {"rhel_release": {"name": "RHEL-7-foobar"}}
 
+        self.builds_by_cve = {
+            "CVE-2020-12345": {
+                "PRODUCT1": {
+                    "name": "PRODUCT1",
+                    "description": "PRODUCT Version 1",
+                    "builds": [
+                        {
+                            "libntirpc-1.4.3-4.el6rhs": {
+                                "nvr": "libntirpc-1.4.3-4.el6rhs",
+                                "nevr": "libntirpc-0:1.4.3-4.el6rhs",
+                                "id": 1220279,
+                                "is_module": False,
+                                "variant_arch": {
+                                    "PRODUCT1": {
+                                        "x86_64": [
+                                            "libntirpc-1.4.3-4.el6rhs.x86_64.rpm"
+                                        ],
+                                        "ppc64le": [
+                                            "libntirpc-1.4.3-4.el6rhs.ppc64le.rpm"
+                                        ],
+                                        "s390x": ["libntirpc-1.4.3-4.el6rhs.s390x.rpm"],
+                                        "aarch64": [
+                                            "libntirpc-1.4.3-4.el6rhs.aarch64.rpm"
+                                        ],
+                                        "SRPMS": ["libntirpc-1.4.3-4.el6rhs.src.rpm"],
+                                    },
+                                    "PRODUCT2": {
+                                        "x86_64": [
+                                            "libntirpc-1.4.3-4.el6rhs.x86_64.rpm"
+                                        ],
+                                        "ppc64le": [
+                                            "libntirpc-1.4.3-4.el6rhs.ppc64le.rpm"
+                                        ],
+                                        "s390x": ["libntirpc-1.4.3-4.el6rhs.s390x.rpm"],
+                                        "aarch64": [
+                                            "libntirpc-1.4.3-4.el6rhs.aarch64.rpm"
+                                        ],
+                                        "SRPMS": ["libntirpc-1.4.3-4.el6rhs.src.rpm"],
+                                    },
+                                },
+                            }
+                        }
+                    ],
+                }
+            }
+        }
+
     def errata_rest_get(self, endpoint):
         if endpoint.find("build/") != -1:
             nvr = endpoint.split("/")[-1]
             return self.builds[nvr]
+        elif endpoint.find("builds_by_cve") != -1:
+            return self.builds_by_cve
         elif endpoint.find("erratum/") != -1:
             return self.advisory_rest_json
 
@@ -161,14 +210,8 @@ class TestErrata(helpers.FreshmakerTestCase):
         super(TestErrata, self).setUp()
         self.errata = Errata("https://localhost/")
 
-        self.patcher = helpers.Patcher(
-            'freshmaker.errata.SFM2API.')
-        self.patcher.patch("fetch_cve_metadata",
-                           return_value=["moderate", {}])
-
     def tearDown(self):
         super(TestErrata, self).tearDown()
-        self.patcher.unpatch_all()
 
     @patch.object(Errata, "_errata_rest_get")
     @patch.object(Errata, "_errata_http_get")
@@ -181,11 +224,10 @@ class TestErrata(helpers.FreshmakerTestCase):
         self.assertEqual(advisories[0].name, "RHSA-2017:28484")
         self.assertEqual(advisories[0].state, "QE")
         self.assertEqual(advisories[0].content_types, ["rpm"])
-        self.assertEqual(advisories[0].security_impact, "Important")
+        self.assertEqual(advisories[0].security_impact, "important")
         self.assertEqual(advisories[0].product_short_name, "product")
         self.assertEqual(advisories[0].cve_list,
                          ["CVE-2015-3253", "CVE-2016-6814"])
-        self.assertEqual(advisories[0].highest_cve_severity, "moderate")
         self.assertEqual(advisories[0].has_hightouch_bug, True)
 
     @patch.object(Errata, "_errata_rest_get")
@@ -342,6 +384,13 @@ class TestErrata(helpers.FreshmakerTestCase):
         }
         ret = self.errata.get_builds(28484, "")
         self.assertEqual(ret, set(['libntirpc-1.4.3-4.el7rhgs']))
+
+    @patch.object(Errata, "_errata_rest_get")
+    @patch.object(Errata, "_errata_http_get")
+    def test_errata_get_cve_affected_srpm_nvrs(self, errata_http_get, errata_rest_get):
+        MockedErrataAPI(errata_rest_get, errata_http_get)
+        ret = self.errata.get_cve_affected_srpm_nvrs(28484)
+        self.assertEqual(ret, ['libntirpc-1.4.3-4.el6rhs'])
 
     def test_get_docker_repo_tags(self):
         with patch.object(self.errata, "xmlrpc") as xmlrpc:

commit 5b60fe28ed66c79f423ea636af4fb9fcd586f8dd

Filter images by builds in advisory for non-CVE advisories
Fixed a regression in 3726cd4, after changing to use CVE mapping
feature from errata, the case of non-CVE advisory triggered rebuilds
will not work, this is a case in E2E testing. Put the code back to
fix it.

freshmaker/handlers/koji/rebuild_images_on_rpm_advisory_change.py
@@ -414,6 +414,11 @@ class RebuildImagesOnRPMAdvisoryChange(ContainerBuildHandler):
         # Get srpm nvrs which are affected by the CVEs in this advisory
         srpm_nvrs = self.event.advisory.affected_srpm_nvrs
 
+        # If there is no CVE affected srpms, this can be non-RHSA advisory,
+        # just rebuild images that have the builds in this advisory installed
+        if not srpm_nvrs:
+            srpm_nvrs = list(errata.get_builds(errata_id))
+
         self.log_info(
             "Going to find all the container images to rebuild as "
             "result of %r update.", srpm_nvrs)