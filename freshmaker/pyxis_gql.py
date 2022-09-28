e375926c13efe2785e827cc4909e1bee5b65a955
Qixiang Wan <qwan@redhat.com>
2022-09-28 18:13:41 +0800

    Add APIs to find images have older rpms installed
    Use Pyxis GraphQL to find images that have specified rpms installed, and
    the installed NVRs are older than specified NVRs.
    
    JIRA: CWFHEALTH-1249

--- freshmaker/pyxis_gql.py
+++ freshmaker/pyxis_gql.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022  Red Hat, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from functools import cached_property
 
@@ -56,6 +75,62 @@ class PyxisGQL:
         """
         return self._client.execute(dsl_gql(DSLQuery(query_dsl)))
 
+    def _get_repo_projection(self):
+        ds = self.dsl_schema
+        projection = [
+            ds.ContainerRepository.release_categories,
+            ds.ContainerRepository.auto_rebuild_tags,
+            ds.ContainerRepository.registry,
+            ds.ContainerRepository.repository,
+        ]
+        return projection
+
+    def _get_image_projection(self, include_rpms=True):
+        ds = self.dsl_schema
+        projection = [
+            ds.ContainerImage.architecture,
+            ds.ContainerImage.brew.select(
+                ds.Brew.build,
+            ),
+            ds.ContainerImage.content_sets,
+            ds.ContainerImage.parent_brew_build,
+            ds.ContainerImage.parsed_data.select(
+                ds.ParsedData.labels.select(
+                    ds.Label.name,
+                    ds.Label.value,
+                ),
+            ),
+            ds.ContainerImage.repositories.select(
+                ds.ContainerImageRepo.registry,
+                ds.ContainerImageRepo.repository,
+                ds.ContainerImageRepo.published,
+                ds.ContainerImageRepo.tags.select(
+                    ds.ContainerImageRepoTag.name,
+                ),
+            ),
+        ]
+
+        # Include rpm manifest data in result, use edges to get the rpm manifest
+        # data because the direct rpm manifest field doesn't include all data
+        if include_rpms:
+            projection.append(
+                ds.ContainerImage.edges.select(
+                    ds.ContainerImageEdges.rpm_manifest.select(
+                        ds.ContainerImageRPMManifestResponse.data.select(
+                            ds.ContainerImageRPMManifest.image_id,
+                            ds.ContainerImageRPMManifest.rpms.select(
+                                ds.RpmsItems.name,
+                                ds.RpmsItems.nvra,
+                                ds.RpmsItems.srpm_name,
+                                ds.RpmsItems.srpm_nevra,
+                            ),
+                        ),
+                    ),
+                )
+            )
+
+        return projection
+
     def find_repositories(self, published=None, release_categories=None, auto_rebuild_tags=None):
         """Get image repositories
 
@@ -98,12 +173,7 @@ class PyxisGQL:
                 ds.ContainerRepositoryPaginatedResponse.page,
                 ds.ContainerRepositoryPaginatedResponse.page_size,
                 ds.ContainerRepositoryPaginatedResponse.total,
-                ds.ContainerRepositoryPaginatedResponse.data.select(
-                    ds.ContainerRepository.release_categories,
-                    ds.ContainerRepository.auto_rebuild_tags,
-                    ds.ContainerRepository.registry,
-                    ds.ContainerRepository.repository,
-                ),
+                ds.ContainerRepositoryPaginatedResponse.data.select(*self._get_repo_projection()),
             )
 
             result = self.query(query_dsl)
@@ -114,7 +184,11 @@ class PyxisGQL:
             # Data is empty when there are no more results
             if not data:
                 break
+
             repositories.extend(data)
+            # If page_size >= total, means all results have been fetched in the first page
+            if result["find_repositories"]["page_size"] >= result["find_repositories"]["total"]:
+                break
             page_num += 1
 
         return repositories
@@ -129,18 +203,14 @@ class PyxisGQL:
         """
         ds = self.dsl_schema
         query_dsl = ds.Query.get_repository_by_registry_path(
-            repository=repository,
-            registry=registry
+            repository=repository, registry=registry
         ).select(
             ds.ContainerRepositoryResponse.error.select(
                 ds.ResponseError.status,
                 ds.ResponseError.detail,
             ),
             ds.ContainerRepositoryResponse.data.select(
-                ds.ContainerRepository.release_categories,
-                ds.ContainerRepository.auto_rebuild_tags,
-                ds.ContainerRepository.registry,
-                ds.ContainerRepository.repository,
+                *self._get_repo_projection(),
             ),
         )
 
@@ -149,3 +219,81 @@ class PyxisGQL:
         if error is not None:
             raise PyxisGQLRequestError(str(error))
         return result["get_repository_by_registry_path"]["data"]
+
+    def find_images_by_installed_rpms(
+        self, rpm_names, content_sets=None, repositories=None, published=None, tags=None
+    ):
+        """Find images which have the provided rpms installed
+
+        :param list rpm_names: List of rpm names
+        :param list content_sets: List of content sets
+        :param list repositories: List of repository paths
+        :param bool published: The published attribution of image
+        :param list tags: List of image tags
+        :return: List of image data
+        :rtype: list
+        """
+        images = []
+
+        query_filter = {}
+        query_filter["and"] = []
+
+        query_filter["and"].append({"rpm_manifest": {"rpms": {"name": {"in": rpm_names}}}})
+
+        if content_sets:
+            query_filter["and"].append({"content_sets": {"in": content_sets}})
+
+        repo_matches = []
+        if isinstance(published, bool):
+            repo_matches.append({"published": {"eq": published}})
+        if repositories:
+            repo_matches.append({"repository": {"in": repositories}})
+        if tags:
+            repo_matches.append({"tags_elemMatch": {"and": [{"name": {"in": tags}}]}})
+        if repo_matches:
+            query_filter["and"].append({"repositories_elemMatch": {"and": repo_matches}})
+
+        ds = self.dsl_schema
+        page_num = 0
+        page_size = 50
+
+        # Iterate all pages
+        while True:
+            query_dsl = ds.Query.find_images(
+                page=page_num,
+                page_size=page_size,
+                filter=query_filter,
+            ).select(
+                ds.ContainerImagePaginatedResponse.error.select(
+                    ds.ResponseError.status,
+                    ds.ResponseError.detail,
+                ),
+                ds.ContainerImagePaginatedResponse.page,
+                ds.ContainerImagePaginatedResponse.page_size,
+                ds.ContainerImagePaginatedResponse.total,
+                ds.ContainerImagePaginatedResponse.data.select(*self._get_image_projection()),
+            )
+
+            result = self.query(query_dsl)
+            error = result["find_images"]["error"]
+            if error is not None:
+                raise PyxisGQLRequestError(str(error))
+            data = result["find_images"]["data"]
+            # Data is empty when there are no more results
+            if not data:
+                break
+
+            # Only keep the rpms we care about, the large rpm manifest data can impact performance
+            for img in data:
+                rpms = img["edges"]["rpm_manifest"]["data"]["rpms"]
+                img["edges"]["rpm_manifest"]["data"]["rpms"] = [
+                    rpm for rpm in rpms if rpm["name"] in rpm_names
+                ]
+            images.extend(data)
+
+            # If page_size >= total, means all results have been fetched in the first page
+            if result["find_images"]["page_size"] >= result["find_images"]["total"]:
+                break
+            page_num += 1
+
+        return images
