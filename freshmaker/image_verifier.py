633aadec8d74ed35fe9df88528277971c4f964ff
Elena Karolina Semanova <82966683+ElenaKarolinaSemanova@users.noreply.github.com>
2023-02-07 21:46:38 +0100

    Change code to use only Pyxis GQL API
    Update image_verifier.py to use APIs in Pyxis GraphQL module
    
    Update image_verifier.py to use APIs in Pyxis GraphQL module, Jira: CWFHEALTH-1255

--- freshmaker/image_verifier.py
+++ freshmaker/image_verifier.py
@@ -22,123 +22,109 @@
 # Written by Jan Kaluza <jkaluza@redhat.com>
 
 from freshmaker import conf
-from freshmaker.lightblue import LightBlue
+from freshmaker.pyxis_gql import PyxisGQL
+from mypy_extensions import TypedDict
+from typing import Dict, List
+
+
+class DataElements(TypedDict):
+    repository: Dict[str, List[str]]
+    images: Dict[str, Dict[str, List[str]]]
 
 
 class ImageVerifier(object):
-
-    def __init__(self, lb=None):
+    def __init__(self, pyxis: PyxisGQL = None):
         """
-        Creates new ImageVerifier.
+        Creates new ImageVerifier. When None, new default PyxisGQL class is created.
 
-        :param LightBlue lb: Lightblue instance to use to verify images.
-            When None, new default Lightblue class is created.
+        :param PyxisGQL pyxis: PyxisGQL instance to use to verify images.
         """
-        self.lb = lb if lb else LightBlue(
-            server_url=conf.lightblue_server_url,
-            cert=conf.lightblue_certificate,
-            private_key=conf.lightblue_private_key)
+        self.pyxis = (
+            pyxis
+            if pyxis
+            else PyxisGQL(
+                url=conf.pyxis_server_url,
+                cert=(
+                    conf.pyxis_certificate,
+                    conf.pyxis_private_key,
+                ),
+            )
+        )
 
     def _verify_repository_data(self, repo):
         """
-        Verifies the Lightblue ContainerRepository data.
+        Verifies the PyxisGQL ContainerRepository data.
         Raises ValueError in case of error.
         """
-        categories = set(conf.lightblue_release_categories)
+        categories = set(conf.container_release_categories)
         repo_categories = repo.get("release_categories", [])
+
         if not set(repo_categories).intersection(categories):
             raise ValueError(
                 "Only repositories with one of %r release_categories can be "
-                "rebuilt, but found %r." % (categories, repo_categories))
+                "rebuilt, but found %r." % (categories, repo_categories)
+            )
 
         if not repo["published"]:
             for exc in conf.unpublished_exceptions:
-                if repo["registry"] == exc["registry"] and repo["repository"] == exc["repository"]:
+                if repo["repository"] == exc["repository"]:
                     break
             else:
                 raise ValueError(
                     "Only published repositories or unpublished exceptions can be rebuilt, but "
                     "this repository is not published.")
 
-        if "auto_rebuild_tags" not in repo:
-            raise ValueError(
-                "The \"auto_rebuild_tags\" in COMET is not set.")
+        if "auto_rebuild_tags" not in repo or repo["auto_rebuild_tags"] is None:
+            raise ValueError('The "auto_rebuild_tags" in COMET is not set.')
 
         if "auto_rebuild_tags" in repo and repo["auto_rebuild_tags"] == []:
             raise ValueError(
-                "The \"auto_rebuild_tags\" in COMET is set to an empty list, this means "
-                "rebuilds of images in this repository are disabled.")
+                'The "auto_rebuild_tags" in COMET is set to an empty list, this means '
+                "rebuilds of images in this repository are disabled."
+            )
 
     def _verify_image_data(self, image):
         """
-        Verifies the Lightblue ContainerImage data.
+        Verifies the PyxisGQL ContainerImage data.
         Raises ValueError in case of error.
         """
         if not image["content_sets"]:
             raise ValueError(
-                "Found image \"%s\" in this repository, but it cannot be rebuilt, because "
-                "the \"content_sets\" are not set for this image." % image.nvr)
+                'Found image "%s" in this repository, but it cannot be rebuilt, because '
+                'the "content_sets" are not set for this image.'
+                % image["brew"]["build"]
+            )
 
-    def _get_repository_from_name(self, repo_name):
+    def _get_repository_from_name(self, repo_name: str):
         """
         Returns the ContainerRepository object based on the Repository name.
         """
-        query = {
-            "objectType": "containerRepository",
-            "query": {
-                "$and": [
-                    {
-                        "field": "repository",
-                        "op": "=",
-                        "rvalue": repo_name
-                    },
+        repos = self.pyxis.find_repositories_by_repository_name(repo_name)
 
-                ]
-            },
-            "projection": [
-                {"field": "*", "include": True, "recursive": True}
-            ]
-        }
-
-        repos = self.lb.find_container_repositories(query, auto_rebuild=False)
         if not repos:
-            raise ValueError("Cannot get repository %s from Lightblue." % repo_name)
-        if len(repos) != 1:
-            raise ValueError("Multiple records found in Lightblue for repository %s." % repo_name)
+            raise ValueError("Cannot get repository %s from Pyxis." % repo_name)
 
-        return repos[0]
-
-    def _get_repository_from_image(self, nvr):
-        """
-        Returns the ContainerRepository object based on the image NVR.
-        """
-        query = {
-            "objectType": "containerRepository",
-            "query": {
-                "$and": [
-                    {
-                        "field": "images.*.brew.build",
-                        "op": "=",
-                        "rvalue": nvr
-                    },
-
-                ]
-            },
-            "projection": [
-                {"field": "*", "include": True, "recursive": True}
-            ]
-        }
-
-        repos = self.lb.find_container_repositories(query)
-        if not repos:
-            raise ValueError("Cannot get repository for image %s from Lightblue." % nvr)
         if len(repos) != 1:
             raise ValueError(
-                "Image %s found in multiple repositories in Lightblue." % nvr)
+                "Multiple records found in Pyxis for image repository %s." % repo_name
+            )
 
         return repos[0]
 
-    def verify_image(self, image_nvr):
+    def _get_repository_from_image(self, image):
+        """
+        Returns the ContainerRepository object based on the image defined by the image NVR.
+        """
+        if "repositories" not in image or not image["repositories"]:
+            raise ValueError("Cannot get repository for image %s from Pyxis." % image["brew"]["build"])
+
+        repos = [repo for repo in image["repositories"] if repo["registry"] != "conf.image_build_repository_registries"]
+        image_repo = repos[0]
+
+        # returns a single repository
+        return self.pyxis.get_repository_by_registry_path(image_repo["registry"], image_repo["repository"])
+
+    def verify_image(self, image_nvr: str) -> Dict[str, List[str]]:
         """
         Verifies the image defined by `image_nvr`.
         Raises ValueError in case of error.
@@ -147,23 +133,22 @@ class ImageVerifier(object):
         :rtype: dict
         :return: Dict with image NVR as key and list of content_sets as values.
         """
-        repo = self._get_repository_from_image(image_nvr)
+        images = self.pyxis.find_images_by_nvr(image_nvr)
+
+        if not images:
+            raise ValueError("No images found for the specified NVR")
+
+        content_sets = set()
+        for image in images:
+            self._verify_image_data(image)
+            content_sets.update(image["content_sets"])
+
+        repo = self._get_repository_from_image(images[0])
         self._verify_repository_data(repo)
 
-        images = self.lb.get_images_by_nvrs([image_nvr], include_rpm_manifest=False)
-        if not images:
-            raise ValueError(
-                "No published images tagged by %r found in repository" % (
-                    repo["auto_rebuild_tags"]))
+        return {images[0]["brew"]["build"]: sorted(content_sets)}
 
-        image = images[0]
-        self._verify_image_data(image)
-
-        return {
-            image.nvr: image["content_sets"]
-        }
-
-    def verify_repository(self, repo_name):
+    def verify_repository(self, repo_name: str) -> DataElements:
         """
         Verifies the images in repository defined by `repo_name`.
         Raises ValueError in case of error.
@@ -178,26 +163,32 @@ class ImageVerifier(object):
         repo = self._get_repository_from_name(repo_name)
         self._verify_repository_data(repo)
 
-        data = {
+        data: DataElements = {
             "repository": {"auto_rebuild_tags": repo["auto_rebuild_tags"]},
             "images": {}
         }
 
-        images = self.lb.find_images_with_included_rpms(
-            [], [], {repo["repository"]: repo}, include_rpm_manifest=False)
+        images = self.pyxis.find_images_by_repository(repo["repository"], repo["auto_rebuild_tags"])
+
+        images_by_nvr: Dict[str, Dict[str, List[str]]] = {}
         for image in images:
             self._verify_image_data(image)
-            image_data = {"content_sets": [], "tags": []}
-            for repodata in image['repositories']:
-                if repodata["repository"] == repo_name:
-                    image_data["tags"] = [t["name"] for t in repodata["tags"]]
-                    break
-            image_data["content_sets"] = image["content_sets"]
-            data["images"][image.nvr] = image_data
+            nvr = image["brew"]["build"]
+            images_by_nvr.setdefault(nvr, {"content_sets": [], "tags": []})
+            # Image tags should be same for all architectures, just set it once
+            if not images_by_nvr[nvr]["tags"]:
+                for repodata in image["repositories"]:
+                    if repodata["repository"] == repo_name:
+                        images_by_nvr[nvr]["tags"] = [t["name"] for t in repodata["tags"]]
+                        break
+            for cs in image["content_sets"]:
+                if cs not in images_by_nvr[nvr]["content_sets"]:
+                    images_by_nvr[nvr]["content_sets"].append(cs)
+            data["images"] = images_by_nvr
 
         if not data["images"]:
             raise ValueError(
-                "No published images tagged by %r found in repository" % (
-                    repo["auto_rebuild_tags"]))
+                "No published images tagged by %r found in repository" % (repo["auto_rebuild_tags"])
+            )
 
         return data
